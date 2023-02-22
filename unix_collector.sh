3bd947d029ee386c8eedd187788d63609fef12c0
op7ic <3172590+op7ic@users.noreply.github.com>
2023-02-22 15:11:02 +0300

    Update unix_collector.sh

--- unix_collector.sh
+++ unix_collector.sh
@@ -772,9 +772,9 @@ then
 		mkdir $OUTPUT_DIR/homedir/home 1> /dev/null 2> /dev/null
 		mkdir $OUTPUT_DIR/homedir/root 1> /dev/null 2> /dev/null
 		mkdir $OUTPUT_DIR/homedir/home-export 1> /dev/null 2> /dev/null
-		tar --exclude=$OUTPUT_DIR --exclude '*.vmdk' --exclude '*.ovf' --exclude '*.ova' --exclude '*.vhd' --exclude '*.vmss' -cvf $OUTPUT_DIR/homedir/home/home.tar /home/ --exclude-from <(find /home/ -size $TAR_MAX_FILESIZE) 2> /dev/null
-		tar --exclude=$OUTPUT_DIR --exclude '*.vmdk' --exclude '*.ovf' --exclude '*.ova' --exclude '*.vhd' --exclude '*.vmss' -cvf $OUTPUT_DIR/homedir/root/root.tar /root/ --exclude-from <(find /home/ -size $TAR_MAX_FILESIZE) 2> /dev/null
-		tar --exclude=$OUTPUT_DIR --exclude '*.vmdk' --exclude '*.ovf' --exclude '*.ova' --exclude '*.vhd' --exclude '*.vmss' -cvf $OUTPUT_DIR/homedir/home-export/home-export.tar /export/home/ --exclude-from <(find /export/home/ -size $TAR_MAX_FILESIZE) 1> /dev/null 2> /dev/null
+		tar --exclude '*.vmdk' --exclude '*.ovf' --exclude '*.ova' --exclude '*.vhd' --exclude '*.vmss' --exclude=$OUTPUT_DIR -cvf $OUTPUT_DIR/homedir/home/home.tar /home/ --exclude-from <(find /home/ -size $TAR_MAX_FILESIZE) 1> /dev/null 2> /dev/null
+		tar --exclude '*.vmdk' --exclude '*.ovf' --exclude '*.ova' --exclude '*.vhd' --exclude '*.vmss' --exclude=$OUTPUT_DIR -cvf $OUTPUT_DIR/homedir/root/root.tar /root/ --exclude-from <(find /root/ -size $TAR_MAX_FILESIZE) 1> /dev/null 2> /dev/null
+		tar --exclude '*.vmdk' --exclude '*.ovf' --exclude '*.ova' --exclude '*.vhd' --exclude '*.vmss' --exclude=$OUTPUT_DIR -cvf $OUTPUT_DIR/homedir/home-export/home-export.tar /export/home/ --exclude-from <(find /export/home/ -size $TAR_MAX_FILESIZE) 1> /dev/null 2> /dev/null
 	fi
 elif [ $PLATFORM = "aix" ]
 then
